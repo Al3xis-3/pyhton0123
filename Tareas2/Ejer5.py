1934db031e8ed7ced8e51e22b8e25697e2eb7e78
Al3xis-3 <alexish2500@gmail.com>
2023-01-28 17:57:09 -0500

    Ejer5

---
+++ Tareas2/Ejer5.py
@@ -0,0 +1,9 @@
+import main.main as main
+a=int(input("Digite el primer valor => "))
+b=int(input("Digite el segundo valor => "))
+main.Division(a,b)
+
+n=int(input("Digite la cantidad de valores => "))
+main.Suma(n)
+
+main.namefile()
